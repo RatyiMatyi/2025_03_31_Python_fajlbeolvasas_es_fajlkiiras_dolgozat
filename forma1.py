d52b09b5e4805f16e9b38b51773ac53d084c6ca7
Oláh kettes Golf Gusztáv <barta.bulcsu@diak.szbi-pg.hu>
2025-03-31 09:14:42 +0200

    ddd
    d

--- forma1.py
+++ forma1.py
@@ -8,9 +8,47 @@ Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
 
 A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
 """
+"""versenyzok_szama = 0
+with open('beolvasando_adatok/f1.txt', 'r', encoding="utf-8") as forras_falj: 
+    for sor in forras_falj:
+        versenyzok_szama += 1
+
+legtobb_nyeres = []
+for sor in forras_falj:
+    adatok = sor.split(',')
+    if adatok[2] > legtobb_nyeres:
+        legtobb_nyeres.append(adatok[2])
+print(legtobb_nyeres)"""
 
 
-print("A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("A legtöbb futamot nyert versenyző: ____")
-print("A legtöbb futamot teljesített versenyző: ____")
-print("Az átlagos futamszám: ____")
+adatok = []
+gyozelmeke = []
+futamoke = []
+atlag_a = 0
+with open('beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adat = sor.strip().split(';')
+        nev = adat[0]
+        csapat= adat[1]
+        gyozelmek = int(adat[2])
+        futamok = int(adat[3])
+        atlag_a = atlag_a + futamok
+        adatok.append(nev)
+        gyozelmeke.append(gyozelmek)
+        futamoke.append(futamok)
+    legtobb_gyozelem = max(gyozelmeke)
+    legtobb_gyozelem_indexe = gyozelmeke.index(legtobb_gyozelem)
+    legtobb_futam = max(futamoke)
+    legtobb_futam_indexe = futamoke.index(legtobb_futam)
+    atlag = atlag_a / len(adatok)
+    atlag = float(atlag)
+    
+    
+
+
+print(f"A beolvasott fájlban összesen {len(adatok)} versenyző szerepel.")
+print(f"A legtöbb futamot nyert versenyző: {adatok[legtobb_gyozelem_indexe], legtobb_gyozelem} ")
+print(f"A legtöbb futamot teljesített versenyző: {adatok[legtobb_futam_indexe]} ")
+print(f"Az átlagos futamszám: {atlag} ")
+
